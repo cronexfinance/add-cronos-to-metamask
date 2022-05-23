e0e9c2230f0d676747de71752bf58da73d2c4cb9
CRONEX.Finance <98422636+cronexfinance@users.noreply.github.com>
2022-05-23 14:10:13 +0530

    Add files via upload

--- addtometamask.js
+++ addtometamask.js
@@ -35,12 +35,73 @@ async function addNetwork(type) {
                     blockExplorerUrls: ['https://cronoscan.com']
                 }]
             }
-        } 
+        }
         window.ethereum.request({
             method: 'wallet_addEthereumChain',
             params
-        }).then(()=>console.log('Success')).catch((error)=>console.log("Error", error.message));
+        }).then(() => console.log('Success')).catch((error) => console.log("Error", error.message));
     } else {
         alert('Unable to locate a compatible web3 browser!');
     }
+}
+
+function addToWallet(type) {
+    var NetID = '25';
+    var NetChainID = '0x19';
+    var NetName = 'Cronos Mainnet';
+
+    if (type === 'web3') {
+        if (typeof ethereum !== 'undefined') {
+            eth = new Web3Eth(ethereum);
+        } else if (typeof web3 !== 'undefined') {
+            eth = new Web3Eth(web3.givenProvider);
+        } else {
+            eth = new Web3Eth(ethereum);
+        }
+    }
+    var network = 0;
+    eth.getChainId((err, netId) => {
+        network = netId.toString();
+        switch (network) {
+            case NetID:
+                network = "Cronos Mainnet";
+                break;
+            default:
+                console.log('This is an unknown network.');
+        }
+
+        if (network.toLowerCase() !== NetName.toLowerCase()) {
+            alert("Please connect to Cronos Chain network");
+            return false;
+        } else {
+            try {
+                web3.currentProvider.sendAsync({
+                    method: 'wallet_watchAsset',
+                    params: {
+                        'type': 'ERC20',
+                        'options': {
+                            'address': '0x5FB33B065Dc1D42192653FfD492235B9dA35Fe59',
+                            'symbol': $.sanitize('CRONEX'),
+                            'decimals': '18',
+                            'image': 'https://github.com/cronexfinance/cronex-token/raw/main/2022-05-15%2020.51.25.jpg',
+                        },
+                    },
+                    id: Math.round(Math.random() * 100000)
+                }, function (err, data) {
+                    if (!err) {
+                        if (data.result) {
+                            console.log('Token added');
+                        } else {
+                            console.log(data);
+                            console.log('Some error');
+                        }
+                    } else {
+                        console.log(err.message);
+                    }
+                });
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    });
 }
